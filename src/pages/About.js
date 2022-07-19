5808c4c6c0541a18df30c29bf5b623cb62b8db10
rubisanto <antoinegogceapb@hotmail.fr>
2022-07-19 10:45:05 +0200

    cleaning + paths

---
+++ src/pages/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti hic
+        quidem pariatur ipsam! Exercitationem rem perspiciatis hic rerum? Fuga
+        excepturi provident rem corrupti ipsum corporis dolore deserunt. Ratione
+        iusto deleniti eius praesentium hic aperiam sint eos commodi quibusdam
+        porro laboriosam quaerat, molestiae, voluptatem quae illo libero magni
+        autem temporibus, inventore saepe esse quos corrupti perferendis. Porro
+        quae facilis aliquid culpa consequuntur. Quod adipisci perspiciatis iure
+        vero debitis quo aspernatur. Porro ullam temporibus possimus sint iure.
+        Magni ea, mollitia, asperiores suscipit fuga sint commodi blanditiis
+        vel, exercitationem ducimus corporis dolores assumenda soluta. Obcaecati
+        officiis modi laborum amet, cum laudantium autem consectetur?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+        voluptatum quam commodi iure laborum ipsa architecto, ipsum, voluptates
+        nostrum ducimus reprehenderit expedita delectus nihil. Distinctio
+        laboriosam blanditiis commodi nihil velit eligendi numquam, laborum
+        quae, maxime sunt magni fugit quam. Vero nisi ex temporibus ipsam saepe
+        facilis harum, inventore, amet ea aliquid minus nam enim quisquam in ut
+        maxime blanditiis hic, alias facere? Inventore consequuntur quo
+        voluptatem voluptatum veritatis tempora. Voluptas cum eum pariatur
+        repudiandae recusandae magnam tempore assumenda accusamus tempora, modi
+        hic eaque doloremque rem consectetur repellendus nihil quo? A facere
+        molestiae saepe sapiente. Autem, itaque consequatur? Culpa vel, rerum
+        saepe adipisci nam beatae repellat doloremque aspernatur necessitatibus
+        odit consectetur fugit cumque pariatur ipsum, placeat ex laudantium
+        recusandae inventore libero. Dolorem sequi expedita neque rerum alias
+        possimus dolor accusantium accusamus officiis sunt facilis, itaque illum
+        praesentium ex sint maxime excepturi quo magnam ratione deleniti sed ad
+        placeat est vero! Assumenda animi eius expedita eligendi quo explicabo
+        enim magni placeat commodi illo dolore obcaecati facilis iure veniam
+        quae non eveniet cumque ex, quidem laudantium libero? Velit hic officia
+        pariatur quia dolor. Perferendis a quos rerum neque consequatur commodi
+        nam illo, odio, at architecto eveniet minus est aut qui labore suscipit
+        id?
+      </p>
+    </div>
+  );
+};
+
+export default About;
