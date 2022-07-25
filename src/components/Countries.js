eee45d1e89ebfe6494a85f6a26c92e4b860d18a6
rubisanto <antoinegogceapb@hotmail.fr>
2022-07-25 12:49:24 +0200

    bugs fixed

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -6,6 +6,10 @@ import Card from "./Card";
 const Countries = () => {
   // déclarer variable
   const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
   // useEffect permet de déclencher une fonction à chaque fois que le composant est monté
   useEffect(() => {
     axios
@@ -15,12 +19,44 @@ const Countries = () => {
 
   return (
     <div className="countries">
-      <h1>Countries</h1>
-      <ul>
-        {data.map((country, index) => (
-          // mise en place d'une props
-          <Card key={index} country={country} />
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              //   enleve le checked quand on a annuler recherche
+              checked={continent === selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            {/* htmlfor est pour faire une boucle for  */}
+            <label htmlFor={continent}>{continent}</label>
+          </li>
         ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          // trier l'affichage des pays par la population
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            // mise en place d'une props
+            <Card key={index} country={country} />
+          ))}
       </ul>
     </div>
   );
